f813472556360b6844847e0a35d1239436bb05a1
Ji won Na <jiwonna52@gmail.com>
2022-03-31 13:20:46 +0900

    test.h에다가 전부 정리.

--- make_test/make_test.cpp
+++ make_test/make_test.cpp
@@ -7,84 +7,29 @@ using namespace std;
 
 int main()
 {
-    /*보기는 총 3개*/
-    /*string str;
-    getline(cin, str);
-    cout << "str: " << str << endl;*/
-    
-    int num = 0;
-    string question = "";
-    string example = "";
-    string answer = "";
-    int c;
-    int correct;
-    string a = "";
 
-    /*cout << "총 몇 문제를 입력하시겠어요?" << endl;
-    cin >> num;
-    cout << endl;*/
+    int c;
 
     test user_question;
 
-    cout << "문제를 입력하시오" << endl;
-    getline(cin, question);
-    user_question.have_question(question);
+    user_question.have_question();
     cout << endl;
 
-    cout << "보기를 입력하시오" << endl;
-    for (int i = 0; i < 3; i++) {
-        cin >> example;
-        user_question.have_example(example);
-    }
+    user_question.have_example();
     cout << endl;
 
-    cout << "정답을 입력하시오:" << endl;
-    cin >> answer;
-    user_question.have_answer(answer);
+    user_question.have_answer();
     cout << endl;
 
     cout << "문제를 풀고 싶으면 1을 입력하세요: " << endl;
     cin >> c;
     if (c == 1) {
-        cout << "문제를 확인합니다..." << endl;
-        Sleep(1000);
-        system("cls");
-
-        user_question.print_q();
-        user_question.print_ex();
-        cout << endl;
-
-        cout << "정답을 입력하세오: ";
-        cin >> a;
-        correct = user_question.solve_answer(a);
-        cout << "당신의 점수는 " << correct;
+        user_question.solve_problem();
     }
     else {
         cout << "No?";
     }
-    
 
-    /*for (int i = 0; i < num; i++) {
-       // cout << "문제를 입력하세요." << endl;
-        getline(cin, question);
-        user_question.have_question(question);
-        cout << endl;
-
-        for (int j = 0; j < 3; j++) {
-            cin >> example;
-            user_question.have_example(example);
-        }
-        cout << endl;
-
-        //cout << "답을 입력하세요" << endl;
-        cin >> answer;
-        user_question.have_answer(answer);
-        cout << endl;
-    }*/
-
-    
-
-    /*총 몇 문제를 낼지 받아서 그만큼 돌려주면서 문제 -> 보기 -> 답 순으로 저장한다.*/
 
 }
 
--- make_test/test.h
+++ make_test/test.h
@@ -20,21 +20,34 @@ protected:
 
 public:
 
-	test(){}
+	test(){
+		correct = 0;
+	}
 	~test(){}
 
 	/*문제를 입력받는 함수
 	문제를 받아가지고 벡터에다가 저장해야 한다.*/
 
-	void have_question(string q) {
+	void have_question() {
+		string q;
+		cout << "문제를 입력하시오" << endl;
+		getline(cin, q);
 		question.push_back(q);
 	}
 
-	void have_example(string ex) {
-		example.push_back(ex);
+	void have_example() {
+		string ex;
+		cout << "보기를 입력하시오" << endl;
+		for (int i = 0; i < 3; i++) {
+			cin >> ex;
+			example.push_back(ex);
+		}
 	}
 
-	void have_answer(string an) {
+	void have_answer() {
+		string an;
+		cout << "정답을 입력하시오: " << endl;
+		cin >> an;
 		answer.push_back(an);
 	}
 
@@ -67,4 +80,25 @@ public:
 		}
 		return correct;
 	}
+
+	void solve_problem() {
+		string s_an;
+		int score;
+
+		cout << "문제를 확인합니다..." << endl;
+		Sleep(1000);
+		system("cls");
+
+		print_q();
+		print_ex();
+		cout << endl;
+
+		cout << "정답을 입력하세오: ";
+		cin >> s_an;
+		score = solve_answer(s_an);
+
+		cout << "당신의 점수는 " << correct;
+	}
+
+
 };

6f0888f5c103a7bda0c0a481a7ac539ff65a769c
Ji won Na <jiwonna52@gmail.com>
2022-04-03 20:01:21 +0900

    문제의 수를 입력하고 그 수만큼 문제를 받아서 풀도록 하는 과정을 포함. getline() 오류 해결. cin.ignore()를 이용하면 된다.

--- make_test/make_test.cpp
+++ make_test/make_test.cpp
@@ -9,22 +9,35 @@ int main()
 {
 
     int c;
-
     test user_question;
+    int num;
+    int current_num = 1;
 
-    user_question.have_question();
-    cout << endl;
+   cout << "풀 문제의 수를 입력하시오: ";
+    cin >> num;
+    system("cls");
 
-    user_question.have_example();
-    cout << endl;
+    for (int i = 0; i < num; i++) {
+        user_question.have_question(current_num);
+        cout << endl;
 
-    user_question.have_answer();
-    cout << endl;
+        user_question.have_example(num);
+        cout << endl;
+
+        user_question.have_answer();
+        cout << endl;
+
+        system("cls");
+
+        current_num++;
+    }
 
     cout << "문제를 풀고 싶으면 1을 입력하세요: " << endl;
     cin >> c;
+
     if (c == 1) {
-        user_question.solve_problem();
+        user_question.solve_problem(num);
+        user_question.show_score();
     }
     else {
         cout << "No?";
@@ -32,14 +45,3 @@ int main()
 
 
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
--- make_test/test.h
+++ make_test/test.h
@@ -14,7 +14,7 @@ using namespace std;
 class test {
 protected:
 	vector<string> question;
-	vector<string> example;
+	vector<vector<string>> example;
 	vector<string> answer;
 	int correct;
 
@@ -28,19 +28,33 @@ public:
 	/*문제를 입력받는 함수
 	문제를 받아가지고 벡터에다가 저장해야 한다.*/
 
-	void have_question() {
+	void have_question(int current_num) {
 		string q;
+		cout << current_num << "번" << endl;
 		cout << "문제를 입력하시오" << endl;
+		cin.ignore();
 		getline(cin, q);
 		question.push_back(q);
 	}
 
-	void have_example() {
+	void have_example(int num) {
 		string ex;
-		cout << "보기를 입력하시오" << endl;
-		for (int i = 0; i < 3; i++) {
-			cin >> ex;
-			example.push_back(ex);
+		vector<string> r_example;
+		int current_num = 0;
+		int ex_num = 1;
+		
+		for (int i = current_num; i < num; i++) {
+			cout << "보기를 입력하시오" << endl;
+			for (int j = 0; j < 3; j++) {
+				cout << ex_num << ") ";
+				cin >> ex;
+				r_example.push_back(ex);
+				ex_num++;
+			}
+			example.push_back(r_example);
+			ex_num = 1;
+			current_num++;
+			break;
 		}
 	}
 
@@ -51,16 +65,16 @@ public:
 		answer.push_back(an);
 	}
 
-	void print_q() {
-		for (int i = 0; i < question.size(); i++) {
-			cout << question[i] << endl;
-		}
+	void print_q(int current, int current_num) {
+		cout << current_num << "번" << endl;
+		cout << question[current] << endl;
 		cout << endl;
 	}
 
-	void print_ex() {
+	void print_ex(int current) {
 		for (int i = 0; i < 3; i++) {
-			cout << example[i] << endl;
+			cout << i+1 << ") ";
+			cout << example[current][i] << endl;
 		}
 		cout << endl;
 	}
@@ -72,32 +86,39 @@ public:
 		cout << endl;
 	}
 
-	int solve_answer(string a) {
-		for (int i = 0; i < answer.size(); i++) {
-			if (a == answer[i]) {
-				correct++;
-			}
+	void solve_answer(string a, int current) {
+		if (a == answer[current]) {
+			correct++;
 		}
-		return correct;
 	}
 
-	void solve_problem() {
+	void show_score() {
+		cout << "당신의 점수는 " << correct;
+	}
+
+	void solve_problem(int num) {
 		string s_an;
 		int score;
+		int i = 0;
+		int current_num = 1;
 
 		cout << "문제를 확인합니다..." << endl;
 		Sleep(1000);
 		system("cls");
 
-		print_q();
-		print_ex();
-		cout << endl;
+		while (i != num) {
+			print_q(i, current_num);
+			print_ex(i);
 
-		cout << "정답을 입력하세오: ";
-		cin >> s_an;
-		score = solve_answer(s_an);
-
-		cout << "당신의 점수는 " << correct;
+			cout << "정답을 입력하세오: ";
+			cin >> s_an;
+			solve_answer(s_an, i);
+			cout << endl;
+			system("cls");
+			i++;
+			current_num++;
+		}
+		
 	}
 
 
